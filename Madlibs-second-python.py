ac81e85210c629c05f5da5b24d3ac6e08fac0617
Bakir Abdulwahab <bakir2005abdulwahab@gmail.com>
2023-12-28 20:35:13 -0800

    Returned to normal

--- Madlibs-second-python.py
+++ Madlibs-second-python.py
@@ -3,6 +3,8 @@ verb = input("Type in a verb (past tense): ")
 color = input("Choose a color: ")
 secondVerb = input("Now type in one last verb (past tense): ")
 
-madlibs = f"Once upon a time, I saw a {noun} so I {verb} to it. It looked very {color}, but then I saw it was dangerous and {secondVerb} out of here."
+loading = "\nLoading...\n\n"
+
+madlibs = f"{loading}Once upon a time, I saw a {noun} so I {verb} to it. It looked very {color}, but then I saw it was dangerous and {secondVerb} out of here."
 
 print (madlibs)
