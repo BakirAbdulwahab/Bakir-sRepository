c554f23ef806d8b484aaa58e82d3b220a52acba8
Bakir Abdulwahab <bakir2005abdulwahab@gmail.com>
2024-01-27 08:42:33 -0800

    A way to calculate someone's BMI.

---
+++ BMI-Calculator.py
@@ -0,0 +1,11 @@
+# # Don't Change the code below!!
+height = input("Enter your height in Meters: ")
+weight = input("Enter your weight in Kilograms: ")
+# # Don't Change the code Above!!
+
+# ###############################
+bmi = int(weight) / float(height) ** 2
+
+bmi_as_int = int(bmi)
+
+print(bmi_as_int)
